0150b750ad47a0a39633802c411801a6de6360a7
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-01 16:47:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thesportspost::Application.config.secret_token = '5a932c250e1e65b6414c1c2a645fd777a9b269dc7bcd33e6cb652e5ce372c146a1db9a9892f27fdcced02cb5bad17a4ae118bed1cc11e2ed1ea8c1bd28b5a8bf'
