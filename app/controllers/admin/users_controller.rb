15af36b3c998456a4f2cc3e6060248b1aaf3e842
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-02 04:28:52 -0700

    More admin stuff.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
-
   def new
+    @user = User.new
   end
 
   def index
@@ -8,5 +8,15 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    p params[:user][:roles_mask].to_i
+    @user.roles_mask = params[:user][:roles_mask].to_i
+    if @user.save
+      redirect_to :action => 'index'
+    end
   end
 end
