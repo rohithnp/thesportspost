a4c09223698c2f3bdedb2947afb7af36cdf29093
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-09 18:41:58 -0800

    Some small css changes and tag fixes for article text.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
   def format_article(text)
-    sanitize(text, :tags => %w(p img iframe strong span h1 h2 h3 h4 h5 h6 ol ul li), :attributes => %w(id class style src width height))
+    sanitize(text, :tags => %w(p img br a iframe strong span h1 h2 h3 h4 h5 h6 ol ul li), :attributes => %w(id class style src width height href))
   end
 end
