87ee0f2a82c903853e6472a303f8f33794397105
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-09 22:49:10 -0800

    Use slidesjs.com slider instead. Replace slider on front page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,7 @@ class ApplicationController < ActionController::Base
 
   def index
     @headlines = Article.limit(7)
-    @slideshow = Article.where('image_file_name IS NOT NULL').limit(3).map {|a|
-      {
-        :src => a.image.url(:big),
-        :link => "/articles/#{a.slug}",
-        :name => a.title
-      }
-    }.to_json
+    @slideshow = Article.where('image_file_name IS NOT NULL').limit(5)
     @posts = Article.limit(5)
   end
 
