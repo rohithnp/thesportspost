75cb8c96aa3816c93e8ab51f7411059eeab61e75
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-13 09:50:32 -0800

    Add stuff to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.create(:email => 'admin@test.com', :password => 'asdfasdf', :password_confirmation => 'asdfasdf')
+u.roles_mask = 1
+u.save
+
 User.create(:email => 'noob@test.com', :password => 'asdfasdf', :password_confirmation => 'asdfasdf')
 
 u = User.create(:email => 'editor@test.com', :password => 'asdfasdf', :password_confirmation => 'asdfasdf')
@@ -15,6 +19,10 @@ u = User.create(:email => 'writer@test.com', :password => 'asdfasdf', :password_
 u.roles_mask = 2
 u.save
 
-u = User.create(:email => 'admin@test.com', :password => 'asdfasdf', :password_confirmation => 'asdfasdf')
-u.roles_mask = 1
-u.save
+
+Category.create(:full_name => 'MMA', :name => 'mma')
+Category.create(:full_name => 'Basketball', :name => 'basketball')
+Category.create(:full_name => 'Football', :name => 'football')
+Category.create(:full_name => 'Baseball', :name => 'baseball')
+
+
