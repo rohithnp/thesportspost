d14edbb555ce9006b7921f3dd3bb9ee274e7710a
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-13 21:13:54 -0800

    Add some validations to category and subcategory.

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -2,6 +2,9 @@ class Subcategory < ActiveRecord::Base
   has_many :articles
   belongs_to :category
   
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
   def to_s
     name
   end
