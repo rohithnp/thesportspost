dec5a21994803ac4c68879da0f60cc5ac7aa39e5
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-13 22:49:00 -0800

    Fix page_section admin. Don't show broken images.

--- app/controllers/admin/page_sections_controller.rb
+++ app/controllers/admin/page_sections_controller.rb
@@ -2,5 +2,13 @@ class Admin::PageSectionsController < ApplicationController
 
   def index
     @page_section = PageSection.first
+    @articles = Article.all[0..100]
+  end
+
+  def update
+    @page_section = PageSection.find(params[:id])
+    if @page_section.update_attributes(params[:page_section])
+      redirect_to :action => :index
+    end
   end
 end
