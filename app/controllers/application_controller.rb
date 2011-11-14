7ba25d9ea73a82d2da93b147acaaac291dd72865
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-13 20:04:51 -0800

    Bunch of css updates.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :get_categories
 
   def index
-    @headlines = Article.limit(4)
+    @headlines = Article.limit(5)
     @slideshow = Article.where('image_file_name IS NOT NULL').limit(5)
     @page_sections = PageSection.all[0..1]
     @posts = Article.limit(8)
