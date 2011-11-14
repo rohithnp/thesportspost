1e96c56df18243d8d4286178d1249a507aeed40a
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-13 20:46:49 -0800

    More css for the homepage.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     @headlines = Article.limit(5)
     @slideshow = Article.where('image_file_name IS NOT NULL').limit(5)
     @page_sections = PageSection.all[0..1]
+    @page_section = PageSection.first
     @posts = Article.limit(8)
   end
 
