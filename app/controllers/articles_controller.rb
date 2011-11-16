ceae1192aaf2c2ffdbd9164bf57d1b6cbd4e9041
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-15 21:20:04 -0800

    Redirect invalid article routes to homepage.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,8 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find_by_slug(params[:id])
     unless @article
-      raise ActionController::RoutingError.new('Not Found')
+      # raise ActionController::RoutingError.new('Not Found')
+      redirect_to '/'
     end
   end
 end
