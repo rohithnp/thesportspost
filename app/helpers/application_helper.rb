f6e640cf434a7af6202c5be16ebe5f7fbb26a986
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-20 20:21:58 -0800

    Add login form to the top bar (if not logged in).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_name
+    :user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
