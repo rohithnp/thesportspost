07e071c6a758cc3b487c72497e189c62ee125685
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-24 13:29:52 -0800

    Auto-add related articles and stuff if they don't exist.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,15 @@ class ArticlesController < ApplicationController
     @article.pageviews ||= 0
     @article.pageviews += 1
     @article.save
+    @related_articles = if @article.related_articles.length > 0
+      @article.related_articles
+    else
+      Article.where(:category_id => @article.category_id).order('created_at DESC').limit(5)
+    end
+    @same_writer_articles = if @article.same_writer_articles.length > 0
+      @article.same_writer_articles
+    else
+      Article.where(:user_id => @article.user_id).order('created_at DESC').limit(5)
+    end
   end
 end
