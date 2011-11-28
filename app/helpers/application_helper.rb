99bce0b4cf3c6bb739f8fb6f22882f7bedab35a2
Linmiao Xu <linmiao.xu@gmail.com>
2011-11-28 01:55:09 -0800

    Add adsense codes as application helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,32 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def adsense_top_right
+    '<script type="text/javascript"><!--
+    google_ad_client = "ca-pub-1214541924352395";
+    /* Top Banner Ad */
+    google_ad_slot = "2983044274";
+    google_ad_width = 336;
+    google_ad_height = 280;
+    //-->
+    </script>
+    <script type="text/javascript"
+    src="">
+    </script>'
+  end
+
+  def adsense_under_headlines
+    '<script type="text/javascript"><!--
+    google_ad_client = "ca-pub-1214541924352395";
+    /* Small Box Under Headlines */
+    google_ad_slot = "1686483643";
+    google_ad_width = 250;
+    google_ad_height = 250;
+    //-->
+    </script>
+    <script type="text/javascript"
+    src="">
+    </script>'
+  end
 end
