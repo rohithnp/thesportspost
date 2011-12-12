01cbfc702e29198c25d867bbabffc2b31227ca2f
Linmiao Xu <linmiao.xu@gmail.com>
2011-12-12 02:22:46 -0800

    Add slideshow model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :articles
   has_many :subcategories
-  
+
   validates_presence_of :name, :full_name
   validates_uniqueness_of [:name, :full_name]
 
---
+++ app/models/slideshow.rb
@@ -0,0 +1,3 @@
+class Slideshow < ActiveRecord::Base
+  serialize :article_ids
+end
